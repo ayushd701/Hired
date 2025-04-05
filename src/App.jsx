a74dc12bb521d988674aab6480085bd64d7ac3f4
ayushd701 <dixitayush0107@gmail.com>
2025-04-05 18:57:40 +0530

    Dark theme added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,59 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './App.css'
-import App_layout from './layout/App_layout'
-import { Job, Job_listing, LandingPage, My_Jobs, Onboarding, Post_job, Saved_jobs } from "./pages/index.js"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import { ThemeProvider } from "./components/theme_provider";
+import App_layout from "./layout/App_layout";
+import {
+  Job,
+  Job_listing,
+  LandingPage,
+  My_Jobs,
+  Onboarding,
+  Post_job,
+  Saved_jobs,
+} from "./pages/index.js";
 
 const router = createBrowserRouter([
   {
     element: <App_layout />,
-    children:[
+    children: [
       {
-        path: '/',
-        element: <LandingPage />
-      } ,
+        path: "/",
+        element: <LandingPage />,
+      },
       {
-        path: '/onboarding',
-        element: <Onboarding />
-      } ,
+        path: "/onboarding",
+        element: <Onboarding />,
+      },
       {
-        path: '/jobs',
-        element: <Job_listing />
-      } ,
+        path: "/jobs",
+        element: <Job_listing />,
+      },
       {
-        path: '/job/:id',
-        element: <Job />
-      } ,
+        path: "/job/:id",
+        element: <Job />,
+      },
       {
-        path: '/post-job',
-        element: <Post_job />
-      } ,
+        path: "/post-job",
+        element: <Post_job />,
+      },
       {
-        path: '/saved-jobs',
-        element: <Saved_jobs />
-      } ,
+        path: "/saved-jobs",
+        element: <Saved_jobs />,
+      },
       {
-        path: '/my-jobs',
-        element: <My_Jobs />
-      } ,
-    ]
-  }
-])
+        path: "/my-jobs",
+        element: <My_Jobs />,
+      },
+    ],
+  },
+]);
 
 function App() {
-
   return (
-    <RouterProvider router={router} />
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- src/layout/App_layout.jsx
+++ src/layout/App_layout.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../components/header";
 
 const App_layout = () => {
   return (
     <div>
-      App_layout
-      <Outlet />
+      <div className="grid-background"></div>
+      <main className="min-h-screen container px-15">
+        <Header />
+        <Outlet />
+      </main>
+      <div className="p-10 text-center bg-gray-800 mt-10">Made by Ayush Dixit</div>
     </div>
-  )
-}
+  );
+};
 
-export default App_layout
+export default App_layout;

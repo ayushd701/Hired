5b020b7417e32716f341b358d91e1acb4beeedca
ayushd701 <dixitayush0107@gmail.com>
2025-04-06 12:13:42 +0530

    Landing page ui completed.

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,4 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "../components/ui/carousel";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+} from "../components/ui/card";
+import {Accordion,AccordionItem,AccordionTrigger,AccordionContent} from "../components/ui/accordion"
+import companies from "../data/companies.json";
+import faqs from "../data/faq.json";
+import Autoplay from "embla-carousel-autoplay";
 
 const LandingPage = () => {
   return (
@@ -15,15 +32,82 @@ const LandingPage = () => {
             />
           </span>
         </h1>
-        <p className="text-gray-300 sm:mt-4 text-xs sm:text-xl">Explore thousands of job listings or find the perfect candidate</p>
+        <p className="text-gray-300 sm:mt-4 text-xs sm:text-xl">
+          Explore thousands of job listings or find the perfect candidate
+        </p>
       </section>
-      <div>
-        {/* buttons */}
-        {/* carousel */}
+
+      <div className="flex gap-6 justify-center">
+        <Link to="/jobs">
+          <Button variant="blue" size="xl">
+            Find Jobs
+          </Button>
+        </Link>
+        <Link to="/post-jo">
+          <Button variant="destructive" size="xl">
+            Post a Job
+          </Button>
+        </Link>
       </div>
-      {/* banner */}
-      <section>{/* cards */}</section>
+
+      {/* carousel */}
+
+      <Carousel
+        plugins={[
+          Autoplay({
+            delay: 2000,
+          }),
+        ]}
+        className="w-full py-10"
+      >
+        <CarouselContent className="flex gap-5 sm:gap-20 items-center">
+          {companies.map(({ name, id, path }) => {
+            return (
+              <CarouselItem key={id} className="basis-1/3 lg:basis-1/6">
+                <img
+                  src={path}
+                  alt={name}
+                  className="h-9 sm:h-14 object-contain"
+                />
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+      </Carousel>
+
+      <img src="/banner.jpeg" alt="" className="w-full" />
+
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-2xl font-bold">
+              For Job Seekers
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Search and apply for jobs , track applications and more.</p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-2xl font-bold">For Employers</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Post jobs , manage applications and find the best candidates.</p>
+          </CardContent>
+        </Card>
+      </section>
       {/* accordian */}
+      <Accordion type="single" collapsible>
+        {faqs.map((faq, index) => {
+          return (
+            <AccordionItem key={index} value={`item-${index + 1}`}>
+              <AccordionTrigger>{faq.question}</AccordionTrigger>
+              <AccordionContent>{faq.answer}</AccordionContent>
+            </AccordionItem>
+          );
+        })}
+      </Accordion>
     </main>
   );
 };

9960c829f48d6de22d45ca9c92742abdf0a6b3c7
ayushd701 <dixitayush0107@gmail.com>
2025-04-07 20:48:59 +0530

    Fetching jobs from supabase

--- src/api/apijobs.js
+++ src/api/apijobs.js
@@ -1,9 +1,24 @@
 import { createSupabaseClientWithToken } from "@/utils/supabase";
 
-export async function getJobs(accessToken) {
+export async function getJobs(
+  accessToken,
+  { location, company_id, searchQuery }
+) {
   const supabase = createSupabaseClientWithToken(accessToken);
 
-  const { data, error } = await supabase.from("jobs").select("*");
+  const { data, error } = await supabase.from("jobs").select("* , company: companies(name , logo_url) , saved: saved_jobs(id)");
+
+  if (location) {
+    query = query.eq("location", location);
+  }
+
+  if (company_id) {
+    query = query.eq("company_id", company_id);
+  }
+
+  if (searchQuery) {
+    query = query.eq("title", `%${searchQuery}%`);
+  }
 
   if (error) {
     console.error("Error fetching jobs", error);
@@ -11,4 +26,3 @@ export async function getJobs(accessToken) {
   }
   return data;
 }
-

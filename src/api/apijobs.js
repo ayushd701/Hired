f9e829cb4c5884147ac75a7e9e01ab444029e8ca
ayushd701 <dixitayush0107@gmail.com>
2025-04-08 12:16:54 +0530

    Toggle saved job fn defined

--- src/api/apijobs.js
+++ src/api/apijobs.js
@@ -6,7 +6,9 @@ export async function getJobs(
 ) {
   const supabase = createSupabaseClientWithToken(accessToken);
 
-  const { data, error } = await supabase.from("jobs").select("* , company: companies(name , logo_url) , saved: saved_jobs(id)");
+  const { data, error } = await supabase
+    .from("jobs")
+    .select("* , company: companies(name , logo_url) , saved: saved_jobs(id)");
 
   if (location) {
     query = query.eq("location", location);
@@ -17,7 +19,7 @@ export async function getJobs(
   }
 
   if (searchQuery) {
-    query = query.eq("title", `%${searchQuery}%`);
+    query = query.ilike("title", `%${searchQuery}%`);
   }
 
   if (error) {
@@ -25,4 +27,31 @@ export async function getJobs(
     return null;
   }
   return data;
+}
+
+export async function saveJob(accessToken, { alreadySaved }, saveData) {
+  const supabase = createSupabaseClientWithToken(accessToken);
+
+  if (alreadySaved) {
+    const { data, error: deleteError } = await supabase
+      .from("saved_jobs")
+      .delete()
+      .eq("job_id", saveData.job_id);
+    if (deleteError) {
+      console.error("Error deleting saved job", deleteError);
+      return null;
+    }
+    return data;
+  } else {
+    const { data, error:insertError } = await supabase
+      .from("saved_jobs")
+      .insert([saveData])
+      .select();
+
+    if (insertError) {
+      console.error("Error inserting to saved jobs", error);
+      return null;
+    }
+    return data;
+  }
 }

2e6e892ee1c44562bcc714f8e0fb09da3caac429
ayushd701 <dixitayush0107@gmail.com>
2025-04-12 01:27:37 +0530

    UI fix

--- src/pages/MyJobs.jsx
+++ src/pages/MyJobs.jsx
@@ -16,7 +16,7 @@ const Myjobs = () => {
   }
   return (
     <div>
-      <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">
+      <h1 className="gradient-title font-extrabold text-4xl sm:text-7xl text-center pb-8">
         {user?.unsafeMetadata?.role === "candidate" ? "My Applications" : "My Jobs"}
       </h1>
       {user?.unsafeMetadata?.role === "candidate" ? <CreatedApplications /> : <CreatedJobs />}

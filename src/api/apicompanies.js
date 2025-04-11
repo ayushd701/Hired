8d2484793c26f96bc583ae9128e4676f278f59bc
ayushd701 <dixitayush0107@gmail.com>
2025-04-11 16:51:47 +0530

    Add company functionality.

--- src/api/apicompanies.js
+++ src/api/apicompanies.js
@@ -1,4 +1,4 @@
-import { createSupabaseClientWithToken } from "@/utils/supabase";
+import { createSupabaseClientWithToken , supabaseUrl } from "@/utils/supabase";
 
 export async function getCompanies(accessToken) {
   const supabase = createSupabaseClientWithToken(accessToken);
@@ -13,3 +13,31 @@ export async function getCompanies(accessToken) {
   }
   return companyData;
 }
+
+export async function addNewCompany(accessToken , _ , companyData) {
+  const supabase = createSupabaseClientWithToken(accessToken);
+
+  const random = Math.floor(Math.random()*90000)
+  const filename = `logo-${random}-${companyData.name}`
+
+  const {error : storageError} = await supabase.storage.from("company-logo").upload(filename , companyData.logo)
+
+  if (storageError) {
+    console.error("Error uploading company logo", storageError);
+    return null;
+  }
+
+  const logo_url =`${supabaseUrl}/storage/v1/object/public/company-logo//${filename}`
+
+  const { data: addCompanyData, error: addCompanyError } = await supabase
+    .from("companies")
+    .insert([{name:companyData.name ,logo_url}])
+    .select()
+
+  if (addCompanyError) {
+    console.error("Error adding new company", addCompanyError);
+    return null;
+  }
+  return addCompanyData;
+}
+
